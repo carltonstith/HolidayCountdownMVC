cd25f0d8fb67d4e1a2f50685b804f1e19a84ec74
Carlton Stith <carlton215@gmail.com>
2020-02-05 12:36:46 -0500

    added README, LICENSE, controller

---
+++ HolidayCountdownMVC/Controllers/GiftController.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HolidayCountdownMVC.Models;
+
+namespace HolidayCountdownMVC.Controllers
+{
+    public class GiftController : Controller
+    {
+        private readonly GiftContext _context;
+
+        public GiftController(GiftContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Gift
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Gifts.ToListAsync());
+        }
+
+        // GET: Gift/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gift = await _context.Gifts
+                .FirstOrDefaultAsync(m => m.GiftId == id);
+            if (gift == null)
+            {
+                return NotFound();
+            }
+
+            return View(gift);
+        }
+
+        // GET: Gift/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Gift/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("GiftId,Item,Recipient,Cost")] Gift gift)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(gift);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(gift);
+        }
+
+        // GET: Gift/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gift = await _context.Gifts.FindAsync(id);
+            if (gift == null)
+            {
+                return NotFound();
+            }
+            return View(gift);
+        }
+
+        // POST: Gift/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("GiftId,Item,Recipient,Cost")] Gift gift)
+        {
+            if (id != gift.GiftId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(gift);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GiftExists(gift.GiftId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(gift);
+        }
+
+        // GET: Gift/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gift = await _context.Gifts
+                .FirstOrDefaultAsync(m => m.GiftId == id);
+            if (gift == null)
+            {
+                return NotFound();
+            }
+
+            return View(gift);
+        }
+
+        // POST: Gift/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var gift = await _context.Gifts.FindAsync(id);
+            _context.Gifts.Remove(gift);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GiftExists(int id)
+        {
+            return _context.Gifts.Any(e => e.GiftId == id);
+        }
+    }
+}
---
+++ HolidayCountdownMVC/README.md
@@ -0,0 +1,61 @@
+# Holiday Countdown MVC
+
+<!--- These are examples. See https://shields.io for others or to customize this set of shields. You might want to include dependencies, project status and licence info here --->
+![GitHub issues](https://img.shields.io/github/issues/carltonstith/HolidayCountdownMVC)
+![GitHub stars](https://img.shields.io/github/stars/carltonstith/HolidayCountdownMVC)
+![GitHub forks](https://img.shields.io/github/forks/carltonstith/HolidayCountdownMVC)
+![Twitter Follow](https://img.shields.io/twitter/follow/carltonstith?style=social)
+
+Holiday Countdown MVC is a gift list keeping application that allows a user to do create, update, store, and delete gifts to buy that special someone.
+
+This application uses .NET Core 3.1 and SQL Server.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+* You have installed the latest version of `.NET Core`
+* You have a `Mac, Linux, or Windows` machine and Visual Studio or Visual Studio for Mac.
+* You have read [C# Documentation](https://docs.microsoft.com/en-us/dotnet/csharp/).
+
+## Installing HolidayCountdownMVC
+
+To install HolidayCountdownMVC, follow these steps:
+
+Linux and macOS:
+```
+git clone
+```
+
+Windows:
+```
+git clone
+```
+## Using HolidayCountdownMVC
+
+To use HolidayCountdownMVC, follow these steps:
+
+```
+git clone
+dotnet restore
+dotnet run
+```
+
+## Contributing to HolidayCountdownMVC
+
+To contribute to <project_name>, follow these steps:
+
+1. Fork this repository.
+2. Create a branch: `git checkout -b <branch_name>`.
+3. Make your changes and commit them: `git commit -m '<commit_message>'`
+4. Push to the original branch: `git push origin <project_name>/<location>`
+5. Create the pull request.
+
+Alternatively see the GitHub documentation on [creating a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
+
+## Contact
+
+If you want to contact me you can reach me at <carlton215@gmail.com> or [@carltonstith](hhttps://twitter.com/carltonstith).
+
+## License
+
+This project uses the following license: [MIT](https://opensource.org/licenses/MIT).
